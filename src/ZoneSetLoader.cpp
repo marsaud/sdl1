4f5cba1d3e0dfb93c509d8e21bf1529a79b67161
Fabrice Marsaud <marsaud.fabrice@neuf.fr>
2014-11-10 18:40:56 +0100

    Minor fix

--- src/ZoneSetLoader.cpp
+++ src/ZoneSetLoader.cpp
@@ -19,16 +19,18 @@ void ZoneSetLoader::load(std::string const& mapFilePath, ZoneSetLoader::ZoneSet 
 
     while(std::getline(zoneMapFile, line))
     {
+        // Handling Windows edited files
+        if ('\r' == *(line.cend()-1))
+        {
+            line.erase(line.end()-1);
+        }
+
         std::istringstream streamLine(line);
         std::string cell;
 
         zoneMap.push_back(ZoneSetLoader::ZoneSetLine(0));
         while (std::getline(streamLine, cell, ';'))
         {
-            if ('\r' == *(cell.cend()-1))
-            {
-                cell.erase(cell.end()-1);
-            }
             zoneMap.rbegin()->push_back(cell);
         }
     }
